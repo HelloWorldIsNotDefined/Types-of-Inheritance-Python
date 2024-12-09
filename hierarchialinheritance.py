cad4eb5bf6c486c2f82bb927fe83683ec7f0ed46
HelloWorldIsNotDefined <158848243+HelloWorldIsNotDefined@users.noreply.github.com>
2024-12-09 14:24:54 +0330

    Update hierarchialinheritance.py
    I improved the readability of the code and fixed the problems

--- hierarchialinheritance.py
+++ hierarchialinheritance.py
@@ -1,31 +1,41 @@
-class person:
+class Person:
     def __init__ (self,name,age):
         self.name=name
         self.age=age
+        
     def display(self):
         print("NAME:",self.name)
         print("AGE:",self.age)
-class teacher(person):
+
+
+class Teacher(Person):
     def __init__(self,name,age,exp,r_area):
-        person.__init__(self,name,age)
+        super().__init__(name,age)
         self.exp=exp
         self.r_area=r_area
+        
     def displaydata(self):
-        person.display(self)
+        super().display()
         print("EXPERIENCE:",self.exp)
         print("RESEARCH AREA:",self.r_area)
-class student(person):
+
+
+class Student(Person):
     def __init__(self,name,age,course,marks):
-        person.__init__(self,name,age)
+        super().__init__(name,age)
         self.course=course
         self.marks=marks
+        
     def displaydata(self):
-        person.display(self)
+        super().display()
         print("COURSE:",self.course)
         print("MARKS:",self.marks)
+
+
 print("TEACHER")
-T=teacher("jaya",43,20,"recommender systems")
-T.displaydata()
+t = Teacher("jaya",43,20,"recommender systems")
+t.displaydata()
+
 print("STUDENT")
-S=student("mani",20,"btech",78)
-S.displaydata()
+s = Student("mani",20,"btech",78)
+s.displaydata()
